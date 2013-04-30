ac9538df63ee5c8208fc09e2062aa2e367d3d98e
Matt Mowers <matt.mowers@gmail.com>
2013-04-30 15:07:50 -0700

    adding socket.io to app

--- web.js
+++ web.js
@@ -1,5 +1,7 @@
 var express = require("express");
 var app = express();
+server = require('http').createServer(app);
+io = require('socket.io').listen(server);
 app.use(express.logger());
 
 app.get('/', function(request, response) {
