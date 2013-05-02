af0b5eb3c4e727f15135b2441317e8a165918e3d
Matt Mowers <matt.mowers@gmail.com>
2013-05-02 15:23:19 -0700

    adding connection to mongodb and 1 insert

--- web.js
+++ web.js
@@ -1,8 +1,43 @@
+var mongo = require('mongodb');
 var express = require("express");
 var app = express();
 server = require('http').createServer(app);
 io = require('socket.io').listen(server);
 app.use(express.logger());
+
+
+var mongoUri = process.env.MONGOLAB_URI || 
+  process.env.MONGOHQ_URL || 
+  'mongodb://localhost/poochatdb'; 
+
+mongo.Db.connect(mongoUri, function (err, db) {
+  if(err){
+    console.log("error connecting to database");
+  }else{
+    console.log("connected to database");
+  }
+  db.collection('posts', function(er, collection) {
+    if(er){
+      console.log("error connecting to collection");
+    }else{
+      console.log("connected to collection");
+      collection.save({'text':'third post', 'time':new Date(), 'room':'Starbucks','red':25, 'green':25, 'blue':25}, {safe: true}, function(insErr,insRs) {
+        if(insErr){
+          console.log("error inserting");
+        }else{
+          console.log("good insertion");
+        }
+      });
+    }
+    //collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
+    //});
+  });
+});
+
+
+
+
+
 
 //what if this is only for one route? how do i only apply logic then?
 var rooms = ['Bathroom','Treehouse'];
